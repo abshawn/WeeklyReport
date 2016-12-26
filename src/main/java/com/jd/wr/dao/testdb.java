2a83c7cc880fe8894db67fc2ec499cbf63d1640a
unknown <zhangnan12@360buyad.local>
2016-12-26 11:59:04 +0800

    first commit

---
+++ src/main/java/com/jd/wr/dao/testdb.java
@@ -0,0 +1,19 @@
+package com.jd.wr.dao;
+
+
+import java.sql.SQLException;  
+
+public class testdb {  
+    public static void main(String[] args) throws SQLException  {  
+        DBManager DB = new DBManager();  
+        String url = "jdbc:mysql://localhost:3306/weekreport?"  
+                + "user=root&password=admin&useUnicode=true&characterEncoding=UTF8";  
+        DB.getConnection(url);//连接数据库  
+        DB.getStatement();  
+        DB.rst = DB.read(DB.stmt, "select * from w_context where id=1");//查找数据  
+        while(DB.rst.next()){  
+            System.out.println(DB.rst.getString(1) + "\t" + DB.rst.getString(2));  
+        }  
+       
+    }  
+}
